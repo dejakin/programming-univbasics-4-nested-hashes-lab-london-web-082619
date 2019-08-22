eb31bd62156ffc4d2b05dc643b96cb5a9fb94dfc
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-22 20:39:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,6 @@ def hopper
      }
 
   programmer_hash[:grace_hopper]
-  programmer_hash[:alan_kay]
 
 end
 
@@ -39,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay]
 end
 
 def dennis_ritchies_language
